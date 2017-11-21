2b69ed69d044bce0a6a163ffb30e7d319aee1e47
gaogao <gaoqichen@ptteng.com>
2017-11-21 17:02:39 +0800

    w
    w

--- js/controller/register.js
+++ js/controller/register.js
@@ -7,6 +7,7 @@ $("#register-button").click(function(){
     console.log("注册 ");
     var register = {username:$("#register-username"),email:$("#register-email"),password:$("#register-pwd"),getGoodsName:$("#register-goods-name"),
     phone:$("#register-phone"),address:$("#register-address")};
+    console.log(register);
     $.ajax({url:"/ajax/register",method:"POST",data:register,success:function(result){
         console.log(result);
         if(result.code ==0){
